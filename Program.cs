81802de0e3831664638041faeb2319fa3adf51d5
Hans Kilian <hans@kilianconsult.dk>
2021-12-01 10:06:34 +0100

    Day 1 and 2

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace advent_of_code_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // new Day1().Run();
+            new Day2().Run();
+        }
+    }
+}
