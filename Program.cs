9a1ba222274e8f1f9911b0a3e603ec8f88557cc8
Hans Kilian <hkkilian@gmail.com>
2021-12-03 15:14:48 +0100

    Day 3

--- Program.cs
+++ Program.cs
@@ -7,13 +7,17 @@ namespace advent_of_code_2021
     {
         static void Main(string[] args)
         {
-            var day1 = new Day1();
-            day1.Part1();
-            day1.Part2();
+            // var day1 = new Day1();
+            // day1.Part1();
+            // day1.Part2();
 
-            var day2 = new Day2();
-            day2.Part1();
-            day2.Part2();
+            // var day2 = new Day2();
+            // day2.Part1();
+            // day2.Part2();
+
+            var day3 = new Day3();
+            // day3.Part1();
+            day3.Part2();
         }
     }
 }
