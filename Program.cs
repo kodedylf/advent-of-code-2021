b380a78b3ed1b0e7c7a0d7b416d9a67bcb45c2ca
Hans Kilian <hkkilian@gmail.com>
2021-12-05 11:26:47 +0100

    Day 5

--- Program.cs
+++ Program.cs
@@ -19,9 +19,13 @@ namespace advent_of_code_2021
             // day3.Part1();
             // day3.Part2();
 
-            var day4 = new Day4();
+            // var day4 = new Day4();
             // day4.Part1();
-            day4.Part2();
+            // day4.Part2();
+
+            var day5 = new Day5();
+            // day5.Part1();
+            day5.Part2();
         }
     }
 }
