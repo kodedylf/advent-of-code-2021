305300452ed513f81046a6ce254a92bcf7406797
Hans Kilian <hkkilian@gmail.com>
2021-12-10 13:15:59 +0100

    Day 10

--- Program.cs
+++ Program.cs
@@ -7,41 +7,9 @@ namespace advent_of_code_2021
     {
         static void Main(string[] args)
         {
-            // var day1 = new Day1();
-            // day1.Part1();
-            // day1.Part2();
-
-            // var day2 = new Day2();
-            // day2.Part1();
-            // day2.Part2();
-
-            // var day3 = new Day3();
-            // day3.Part1();
-            // day3.Part2();
-
-            // var day4 = new Day4();
-            // day4.Part1();
-            // day4.Part2();
-
-            // var day5 = new Day5();
-            // day5.Part1();
-            // day5.Part2();
-
-            // var day6 = new Day6();
-            // day6.Part1();
-            // day6.Part2();
-
-            // var day7 = new Day7();
-            // day7.Part1();
-            // day7.Part2();
-
-            // var day8 = new Day8();
-            // day8.Part1();
-            // day8.Part2();
-
-            var day9 = new Day9();
-            day9.Part1();
-            day9.Part2();
+            var day = new Day10();
+            day.Part1();
+            day.Part2();
         }
     }
 }
